d560dbf2fd5aa7bb6ab20b176bbf218274f859c9
Valentin <107222626+mokarey@users.noreply.github.com>
2023-06-07 11:20:12 -0300

    Pruebas unitarias FUNCIONANDO

--- juegosCom/juegosApp/tests.py
+++ juegosCom/juegosApp/tests.py
@@ -1,3 +1,49 @@
 from django.test import TestCase
+from django.urls import reverse
+from perfiles.models import User
 
-# Create your tests here.
+
+#PRUEBA CREAR JUEGO
+class CrearJuegoTestCase(TestCase):
+    def setUp(self):
+        self.username = 'testusuario'
+        self.password = 'testpassword'
+        self.user = User.objects.create_user(username=self.username, password=self.password)
+
+    def test_crear_juego(self):
+        self.client.login(username=self.username, password=self.password)
+
+        url = reverse('crear_juego') 
+        data = {
+            'nombre': 'GTA',
+            'descripcion': 'Descripcion',
+            'genero': 'Accion Disparos',
+            'lanzamiento': '2013-09-17',
+            'clasificacion': 'A',
+            'costo': 2000
+        }
+
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 200)
+
+#PRUEBA CREAR JUEGO GARTIS
+class CrearJuegoGratisTestCase(TestCase):
+    def setUp(self):
+        self.username = 'testusuario'
+        self.password = 'testpassword'
+        self.user = User.objects.create_user(username=self.username, password=self.password)
+
+    def test_crear_juego_gratis(self):
+        self.client.login(username=self.username, password=self.password)
+
+        url = reverse('crear_juego_gratis') 
+        data = {
+            'nombre': 'GTA III',
+            'descripcion': 'Descripcion',
+            'genero': 'Accion Disparos',
+            'lanzamiento': '2013-09-17',
+            'clasificacion': 'A',
+        }
+
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 200)
--- juegosCom/perfiles/tests.py
+++ juegosCom/perfiles/tests.py
@@ -1,3 +1,40 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+#PRUEBA REGISTRO
+class RegistroTestCase(TestCase):
+    def test_registro(self):
+        url = reverse('registro')
+        data = {
+            'username': 'testusuario',
+            'password1': 'testpassword',
+            'password2': 'testpassword'
+        }
+
+        response = self.client.post(url, data)
+
+        self.assertEqual(response.status_code, 302)  
+        self.assertRedirects(response, reverse('inicio'))  
+        self.assertTrue(User.objects.filter(username='testusuario').exists())
+
+#PRUEBA LOGIN
+class LoginViewTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.username = 'testusuario'
+        self.password = 'testpassword'
+        self.user = User.objects.create_user(username=self.username, password=self.password)
+
+    def test_login_view(self):
+        url = reverse('login')
+        login_data = {
+            'username': self.username,
+            'password': self.password
+        }
+
+        response = self.client.post(url, login_data)
+
+        self.assertEqual(response.status_code, 302)  
+        self.assertTrue(response.wsgi_request.user.is_authenticated) 
+
